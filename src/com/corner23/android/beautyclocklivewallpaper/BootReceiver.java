5355f4b42dec0d99445f6d7ab4f315ee1999bfaa
Shaka Huang <shaka@cs03.org>
2010-07-29 17:56:21 +0800

    Add boot receiver for dead wallpaper service

---
+++ src/com/corner23/android/beautyclocklivewallpaper/BootReceiver.java
@@ -0,0 +1,25 @@
+package com.corner23.android.beautyclocklivewallpaper;
+
+import com.corner23.android.beautyclocklivewallpaper.services.DeadWallpaper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+public class BootReceiver extends BroadcastReceiver {
+	
+	public void onReceive(Context context, Intent intent) {
+		if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+			SharedPreferences prefs = context.getSharedPreferences(Settings.SHARED_PREFS_NAME, 0);
+			if (prefs != null) {
+				boolean enableDeadWallpaperService = prefs.getBoolean(Settings.PREF_ENABLE_DEADWALLPAPER, false);
+				if (enableDeadWallpaperService) {
+		    		Intent i = new Intent(context, DeadWallpaper.class);
+		    		i.putExtra("enable", true);
+		    		context.startService(i);
+				}
+			}
+		}
+	}
+}
