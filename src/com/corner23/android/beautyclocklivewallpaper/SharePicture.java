88663aef1d66d3f2ac5b065dcf6cc56c60fd03c8
Shaka Huang <shaka@cs03.org>
2010-09-23 14:41:38 +0800

    [New Feature] Share wallpaper widget

---
+++ src/com/corner23/android/beautyclocklivewallpaper/SharePicture.java
@@ -0,0 +1,82 @@
+package com.corner23.android.beautyclocklivewallpaper;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class SharePicture extends Activity {
+	
+	private File PictureFile = null;
+
+	private Bitmap LoadCurrentPicture() {
+		Time mTime = new Time();
+		mTime.setToNow();
+		
+		SharedPreferences mPrefs = getSharedPreferences(Settings.SHARED_PREFS_NAME, 0);
+		String mStorePath = mPrefs.getString(Settings.PREF_INTERNAL_PICTURE_PATH, "");
+		
+		String sFilePath = String.format("%s/%02d%02d.jpg", mStorePath, mTime.hour, mTime.minute);
+		PictureFile = new File(sFilePath);
+		if (PictureFile.exists()) {
+			return BitmapFactory.decodeFile(sFilePath);
+		}
+		
+		sFilePath = String.format("%s/%02d%02d.jpg", getCacheDir().getAbsolutePath(), mTime.hour, mTime.minute);
+		PictureFile = new File(sFilePath);
+		if (PictureFile.exists()) {
+			return BitmapFactory.decodeFile(sFilePath);
+		}
+		
+		PictureFile = null;
+		
+		return null;
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.share_layout);
+		
+		ImageView iv = (ImageView) findViewById(R.id.ShareImageView);
+		Bitmap pic = LoadCurrentPicture();
+		if (pic != null) {
+			iv.setImageBitmap(pic);
+		} else {
+			Toast.makeText(this, R.string.share_picture_failed_text, Toast.LENGTH_SHORT).show();
+		}
+		
+		Button btn = (Button) findViewById(R.id.ShareButton);
+		btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (PictureFile != null) {
+					Intent shareIntent = new Intent(Intent.ACTION_SEND);
+					shareIntent.setType("image/jpeg");
+					shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(PictureFile));
+					shareIntent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.share_picture_subject_text));
+					shareIntent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.share_picture_msg_text));
+					shareIntent.putExtra(Intent.EXTRA_TITLE, getResources().getString(R.string.share_picture_title_text));
+					startActivity(shareIntent);
+				} else {
+					Toast.makeText(SharePicture.this, R.string.share_picture_failed_text, Toast.LENGTH_SHORT).show();
+				}
+				
+				SharePicture.this.finish();
+			}
+		});
+	}
+}
---
+++ src/com/corner23/android/beautyclocklivewallpaper/widget/ShareWidgetProvider.java
@@ -0,0 +1,48 @@
+package com.corner23.android.beautyclocklivewallpaper.widget;
+
+import com.corner23.android.beautyclocklivewallpaper.R;
+import com.corner23.android.beautyclocklivewallpaper.SharePicture;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class ShareWidgetProvider extends AppWidgetProvider {
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		
+		final int N = appWidgetIds.length;
+		
+		// Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+        	int appWidgetId = appWidgetIds[i];
+
+			Intent intent = new Intent(context, SharePicture.class);
+			PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+			
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.share_widget_layout);
+			views.setOnClickPendingIntent(R.id.ShareButton, pi);
+			
+			// Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		final String action = intent.getAction(); 
+	    if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) { 
+	        final int appWidgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID); 
+	        if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) { 
+	            this.onDeleted(context, new int[] { appWidgetId }); 
+	        } 
+	    } else { 
+	        super.onReceive(context, intent); 
+	    } 
+	}
+}
